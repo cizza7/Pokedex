3e06b5ab7347940c9ed3dda7d7a2e0f2fdb981c2
bboygus <gas-o@hotmail.it>
2017-03-09 14:24:13 +0100

    Aggiunti commenti ed effettuate modifiche secondarie

--- index.php
+++ index.php
@@ -1,44 +1,79 @@
 <?php
 
+  // Apro la connessione col server
+  $conn = mysqli_connect( "localhost", "root", "" );
+
+  // Controllo che la connessione sia avvenuta correttamente
+  if( !$conn ) die( 'Errore durante la connesione' . mysql_error() );
+		
+  // Mi connetto al database corretto sul server
+  $db = mysqli_select_db( $conn, 'pokedex' );
+
+  // Verifico che la connessione al database sia avvenuta correttamente
+  if( !$db ) die( 'Accesso al database non riuscito' . mysql_error() );
+
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 
-    <title> Home </title>
+  <title> Home </title>
 
-    <script src="js/common.js"> </script>
-    <link rel="stylesheet" href="css/common.css" />
-    <link rel="stylesheet" href="css/index.css"  />
+  <!-- Linko gli script e i fogli di stile comuni a tutte le pagine -->
+  <script src="js/common.js"> </script>
+  <link rel="stylesheet" href="css/common.css" />
+
+  <!-- Linko il foglio di stile che descrive gli elementi presenti solo nell'index -->
+  <link rel="stylesheet" href="css/index.css"  />
 
 </head>
 <body>
 
-    <!-- LA NAVBAR COMUNE A TUTTE LE PAGINE -->
-    <ul class="topnav" id="myTopnav">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="php/search.php">Cerca pokemon</a></li>
-        <li><a href="php/login.php">Login</a></li>
-        <li><a href="php/registrati.php">Registrati</a></li>
-        <li><a href="docum.html">Documentazione</a></li>
-        <li class="icon">
-            <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
-        </li>
-    </ul>
+  <!-- LA NAVBAR COMUNE A TUTTE LE PAGINE -->
+  <!-- La navbar è fatta mediante una lista non ordinata in cui gli 
+        elementi sono link alle altre pagine del sito -->
+  <ul class="topnav" id="myTopnav">
+    <li><a href="index.php">Home</a></li>
+    <li><a href="php/search.php">Cerca pokemon</a></li>
+    <li><a href="php/login.php">Login</a></li>
+    <li><a href="php/registrati.php">Registrati</a></li>
+    <li><a href="docum.html">Documentazione</a></li>
+    <li class="icon">
+      <a href="javascript:void(0);" onclick="myFunction()">&#9776;</a>
+    </li>
+  </ul>
 
-    <!-- IL CORPO DELL?ENCICLOPEDIA -->
-    <div id="dex-body">
+  <!-- IL CORPO DELL'ENCICLOPEDIA -->
+  <div id="dex-container">
 
-        <img id="pokedex-up" src="img/up.png" alt="there was an error" />
-        <div>
+    <!-- Il lato sinistro del pokedex -->
+    <img id="pokedex-left" class="initial-values" src="img/up.png" alt="there was an error" />
 
-<!-- TODO: Inserire il corpo vero e proprio dell'enciclopedia -->
+    <!-- La lista vera e propria, riempita coi dati contenuti nel database -->
+    <span id="dex-body">
+      <ol>
+        <?php 
 
-        </div>
-        <img id="pokedex-down" src="img/down.png" alt="there was an error" />
+          // Eseguo la query che restituisce tutti i pokemon
+          $query = "SELECT nome FROM pokemon";
+          $risultato = mysqli_query( $conn, $query ) or die( "Errore nella query: " . mysqli_error( $conn ) );
 
-    </div>
+          // Stampo la lista di tutti i pokemon
+          while( $riga = mysqli_fetch_array( $risultato ) )
+            echo "<li> <a href=\"\"> " . $riga['nome'] . "</a></li>";
+
+          // Chiudo la connessione al database
+          mysql_close( $conn );
+
+              ?>
+          </ol>
+      </span>
+
+      <!-- Il lato destro del pokedex -->
+      <img id="pokedex-right" class="initial-values" src="img/down.png" alt="there was an error" />
+
+  </div>
 
 </body>
 </html>
